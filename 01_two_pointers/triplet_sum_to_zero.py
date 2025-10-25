dad835e9cc6aeeeb09145675920d8b1d11e36958
Akash Kolte <kolteakash07@gmail.com>
2025-10-25 17:39:04 -0400

    boiler plate code

---
+++ 01_two_pointers/triplet_sum_to_zero.py
@@ -0,0 +1,21 @@
+"""
+Problem: <Write problem name or link if from LeetCode/Grokking/GeeksForGeeks>
+
+Pattern: <Algorithm Pattern Name>
+
+Approach:
+1. Explain your intuition
+2. Write time and space complexity
+3. Mention any edge cases
+
+Example:
+Input: ...
+Output: ...
+"""
+
+def solve():
+    pass
+
+
+if __name__ == "__main__":
+    print(solve())
